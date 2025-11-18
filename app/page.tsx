f4e0d6e7d79bd7284ba3bbf4bfb55df0e72e4b3b
Akshat Gupta <akshatx03x@gmail.com>
2025-11-18 20:44:35 +0530

    Deployment fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import App from "../client/src/App"
+import dynamic from 'next/dynamic'
+
+const App = dynamic(() => import("../client/src/App"), { ssr: false })
 
 export default function SyntheticV0PageForDeployment() {
   return <App />
